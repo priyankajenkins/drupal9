dcf4cbd8fa3c63686a1371604571e764c21f7683
priyanka.attarde <priyanka.attarde@infobeans.com>
2023-08-28 17:13:42 +0530

    Aded file'

---
+++ myfirst.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "hello<br>1";
+echo "This is my first jenkin configuration- update3";
+    echo "new test"; 
+        //if
+/*test*/
+
+echo "test at Agu 23";
+echo "test2";
+echo "test3";
