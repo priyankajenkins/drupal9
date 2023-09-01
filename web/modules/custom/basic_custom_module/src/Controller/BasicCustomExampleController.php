c87a3fc12dc3c348ca0c0dc137781230e1482040
priyanka.attarde <priyanka.attarde@infobeans.com>
2023-09-01 16:29:27 +0530

    updated

--- web/modules/custom/basic_custom_module/src/Controller/BasicCustomExampleController.php
+++ web/modules/custom/basic_custom_module/src/Controller/BasicCustomExampleController.php
@@ -6,22 +6,25 @@ use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Controller for the Basic Custom module example.
+ *
  * @author Your Name.
  */
-class BasicCustomExampleController extends ControllerBase {
+class BasicCustomExampleController extends ControllerBase
+{
 
-  /**
-   * Hello World controller method.
-   *
-   * @return array
-   *   Return just an array with a piece of markup to render in screen.
-   */
-  public function helloWorld() {
+    /**
+     * Hello World controller method.
+     *
+     * @return array
+     *   Return just an array with a piece of markup to render in screen.
+     */
+    public function helloWorld()
+    {
 
-    return [
-      '#markup' => $this->t('Hello World, I am just a basic custom example.'),
-    ];
-  }
+        return [
+        '#markup' => $this->t('Hello World, I am just a basic custom example.'),
+        ];
+    }
 
 }
 
